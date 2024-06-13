b2ddf562f25d6730e79f20d2f06c069e88eeaeaf
vigRudolf <vrudi68@gmail.com>
2024-06-13 11:05:54 +0200

    MapElement record

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,14 @@ public class Main {
     public static void main(String[] args) {
         NumberIdentificationFromLine nifl = new NumberIdentificationFromLine();
         //System.out.println(nifl.setLineAndGetNumbers("10 3322 666666655 jklfdj dfkdljfd54 fjdlkfj433"));
-        MyFileScanner fileSc = new MyFileScanner("AdventOfCode5input.txt");
+        /*MyFileScanner fileSc = new MyFileScanner("AdventOfCode5input.txt");
         ArrayList<String> lines =  fileSc.getLines();
         for (String line : lines){
             System.out.println(line);
         }
-
-
+        */
+        //MapElement me = new MapElement(100000L, 10000L, 22L );
+        //System.out.println(me.findDestination(10022L));
+        
     }
 }
--- src/MapElement.java
+++ src/MapElement.java
@@ -1,2 +1,8 @@
-public record MapElement() {
+public record MapElement(Long destinationStart, Long sourceStart, Long range) {
+    public Long findDestination(Long source){
+        long sourceEnd = sourceStart + range - 1;
+        if (sourceStart > source || sourceEnd < source)  return -1L;
+
+        return destinationStart + (source - sourceStart);
+    }
 }
